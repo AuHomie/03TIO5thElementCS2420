88c388e66193f4ed28b61491679534120553d140
Ausha Homer <Aushahomer@weber.mail.edu>
2025-07-11 14:23:32 -0600

    worked on 2nd function. getting help.

--- src/FifthElement.h
+++ src/FifthElement.h
@@ -6,39 +6,64 @@
 
 //template <class Type>
 
-/*
+
 template <class Type>
-class FifthElement(){
+class FifthElement : public LinkedList<Type>{
+
     public:
+
+    Type getFifthElement(){
+
+    Node<Type>* currentNode = front;
+
+    if (count < 5 ){
+    throw length_error("There is no fifth element");
+    }else {
+        for (int i =0; i < 4; i++){
+        }
+        return currentNode->data;}
+    }
+
+    void insertNewFifthElement(const Type &value){
+       
+        Node<Type>* temp = new Node<Type>();  
+        temp->data = value;                   
+        Node<Type>* currentNode = front;
+        
+        if(count < 4 ){
+            throw length_error("There is not that many elements in this list");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            currentNode = currentNode->next;
+            temp->next = currentNode->next;
+            currentNode->next = temp;
+        }
+
+        if(temp->next == nullptr){
+         back = temp;
+        }
+        count ++;
+    }
+
+}
+/*
+This method inserts a node containing a value between the existing 4 and 5 nodes 
+so that the original 5th node becomes the 6th node in the list. If there are only 4 nodes in the list, the new node will become the last node in the list. 
+*/
 
     protected: 
 
 
-}
+};
 
-template<class Type>
-class FifthElement :: public LinkedList {
 
-template<class Type>
-Type getFifthElement(){
-if ( list size !=5 ){
-throw length_error("There is no fifth element";
-}else {
-return linkedList[6]}
-}
+/*
 
-This method returns the data at the fifth node (not index) of the linked list.
-It will throw a length error (i.e. throw length_error("There is no fifth element");) if there is no fifth element in the list
-********
 
-template<class Type>
-void insertNewFifthElement(const Type &value){
-addafter(linkedList)
-}
 
-This method inserts a node containing a value between the existing 4 and 5 nodes 
-so that the original 5th node becomes the 6th node in the list. If there are only 4 nodes in the list, the new node will become the last node in the list. 
-******
+
+*****
 
 template<class Type>
 void deleteFifthElement(){
